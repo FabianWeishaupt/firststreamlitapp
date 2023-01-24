c18dc6eca9d13a6394de7835d675b89e6b8432f1
FabianWeishaupt <52456450+FabianWeishaupt@users.noreply.github.com>
2023-01-24 09:14:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new burger place')
 
-streamlit.header(🥣 'Breakfast Menu')
-streamlit.text(🥗 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🐔 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🥑🍞'Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
